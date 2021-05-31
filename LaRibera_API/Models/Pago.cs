f1e6b9f9c7c3ae9f61712b3dd29c80b8b83a6add
gastonlopez5 <43733902+gastonlopez5@users.noreply.github.com>
2021-05-31 20:18:44 -0300

    Modelos completos

---
+++ LaRibera_API/Models/Pago.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaRibera_API.Models
+{
+    public class Pago
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Comprobante { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public int PedidoId { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+
+        [ForeignKey("PedidoId")]
+        public Pedido Pedido { get; set; }
+    }
+}
